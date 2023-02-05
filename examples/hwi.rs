fa8b965c39ff62b5315180842f328c44a4b43011
Pavol Rusnak <pavol@rusnak.io>
2023-02-05 13:10:56 +0100

    update examples/hwi.rs to include Trezor support

--- examples/hwi.rs
+++ examples/hwi.rs
@@ -6,6 +6,9 @@ use async_hwi::specter::{Specter, SpecterSimulator};
 #[cfg(feature = "ledger")]
 use async_hwi::ledger::{Ledger, LedgerSimulator};
 
+#[cfg(feature = "trezor")]
+use async_hwi::trezor::{Trezor, TrezorSimulator};
+
 #[tokio::main]
 pub async fn main() {
     let list = list_hardware_wallets().await;
@@ -48,6 +51,16 @@ pub async fn list_hardware_wallets() -> Vec<Box<dyn HWI + Send>> {
 
     #[cfg(feature = "ledger")]
     if let Ok(device) = Ledger::try_connect_hid() {
+        hws.push(device.into());
+    }
+
+    #[cfg(feature = "trezor")]
+    if let Ok(device) = TrezorSimulator::try_connect().await {
+        hws.push(device.into());
+    }
+
+    #[cfg(feature = "trezor")]
+    if let Ok(device) = Trezor::try_connect_usb().await {
         hws.push(device.into());
     }
 
